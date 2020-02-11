d622561f219af7569e7aa26b49cc432dc5b0c591
LinkChenzy <linkchenzy@gmail.com>
2020-02-11 17:59:24 +0800

    feat: flower && gray code

--- leetcode/array.js
+++ leetcode/array.js
@@ -112,3 +112,99 @@ const hasGroupsSizeX_2 = (deck) => {
     return group.length ? group[0]>=2 : false
 }
 // console.log(hasGroupsSizeX_2([1,2,3,4,4,3,2,1]))
+
+
+/**
+ * @param {array,number} flowerbed,n
+ * @return {boolean}
+ * @url https://leetcode.com/problems/can-place-flowers/
+ */
+const canPlaceFlowers = function(flowerbed,n){
+    // solved 1
+    let max = 0;
+    for(let i = 0;i<flowerbed.length;i++){
+        if(flowerbed[i]===0){
+            if(i===0&&flowerbed[1]!==1){
+                max ++;
+                i++;
+            }else if(flowerbed[i-1]!==1&&flowerbed[i+1]!==1){
+                max ++;
+                i++;
+            }else if(i === flowerbed.length-1 &&flowerbed[flowerbed.length-2] !== 1 ){
+                max ++;
+                i++;
+            }
+        }
+    }
+    return max>=n;
+    // solved 2
+    for(let i=0;i<flowerbed.length;i++){
+        if(max >= n) return true;
+        if(flowerbed[i]===0 && flowerbed[i-1] !== 1 && flowerbed[i+1] !== 1){
+            max++;
+            i++;
+        }
+    }
+    return max >= n;
+    // solved 3
+    for(let i=0;i<flowerbed.length;i++){
+        //提前完成任务的
+        if(n <= 0) return true
+        //获取左右中的数值
+        const left = i > 0 ? flowerbed[i-1]:0,
+        right = i < flowerbed.length - 1 ? flowerbed[i+1]:0,
+        current = flowerbed[i];
+        if(!(left+right+current)){
+            //符合条件的n-- 中间的数值变成1
+            n--;
+            flowerbed[i] = 1;
+        }
+    }
+    return n <= 0;
+}
+// console.log(canPlaceFlowers([0],1))
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ * @url https://leetcode.com/problems/gray-code/
+ */
+var grayCode = function(n) {
+    // solved 1
+    // 递归函数 借助上一位返回值，根据对称性最后添加一位加入0和1，或者开头添加一位0和1
+    // let turnGray = (n)=>{
+    //     if(n===0){
+    //         return ['0']
+    //     }else if(n===1){
+    //         return ['0','1']
+    //     }else{
+    //         const prev = turnGray(n-1),result = [],max = Math.pow(2,n)-1;
+    //         for(let i=0,len=prev.length;i<len;i++){
+    //             result[i] = `${prev[i]}0`;
+    //             result[max-i] = `${prev[i]}1`
+    //         }
+    //         return result
+    //     }
+    // }
+    // // 二进制转化为十进制
+    // let decimal = (arr) => {
+    //     let newArr = []
+    //     arr.forEach(i=>{
+    //         newArr.push(parseInt(i.toString(),2))
+    //     })
+    //     return newArr
+    // }
+    // return decimal(turnGray(n))
+    // solved 2
+    // 输出的十进制的数组两层循环增加不同的数字，相当于对称加上某一个相同的数字Math.pow(2,n-1)
+    // [0,1]=>[0,1,3,2]=>[0,1,3,2,6,7,5,4]
+    let arr = [0]
+    for(let i=1;i<=n;i++){
+        const num = Math.pow(2,i-1);
+        for(let j=num-1;j>=0;j--){
+            arr.push(num+arr[j])
+        }
+    }
+    return arr
+};
+// console.log(grayCode(3))

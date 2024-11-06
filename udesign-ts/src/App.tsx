33ac841be3f6cc93b573480402b2dbf9afd47f70
linkinzhiyuan <linkinzhiyuan@gmail.com>
2024-11-06 17:43:36 +0800

    feat: udesign init

---
+++ udesign-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import ButtonDemo from './components/Button/demo/basic';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonDemo />
+    </div>
+  );
+}
+
+export default App;
